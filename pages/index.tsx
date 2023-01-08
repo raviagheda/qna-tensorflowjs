587e65ccea1ca069becb6b7f1d648a33f544bfc7
Ravi Agheda <rsagheda123@gmail.com>
2023-01-08 16:31:50 +0530

    added fingerprint

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,15 @@ export default function Home() {
           </Text>
         </div>
         <div className='mt-3'>
-            <QNA />
-          </div>
+          <QNA />
+        </div>
+
+
+        <footer className="fixed bottom-0 right-0 z-20 p-1 bg-white rounded-lg md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <Text className=" text-right text-sm text-gray-500 sm:text-center dark:text-gray-400">@ 2023 <a href="https://raviagheda.vercel.app/" className="hover:underline">RSA</a>
+          </Text>
+        </footer>
+
       </main>
     </>
   )
