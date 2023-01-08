85c657be31ed211cd1c7d766864696216ce2f6ec
Ravi Agheda <rsagheda123@gmail.com>
2023-01-08 16:18:55 +0530

    fix: build errors

--- components/QNA.tsx
+++ components/QNA.tsx
@@ -1,4 +1,4 @@
-import { Container, Input, Link, Loading, Modal, Popover, Textarea } from "@nextui-org/react";
+import { Container, Input, Link, Loading, Modal, Textarea } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Button, Text } from "@nextui-org/react";
 import * as QNAModel from '@tensorflow-models/qna';
@@ -7,8 +7,7 @@ import JSONPretty from 'react-json-pretty';
 import { IAnswer } from "../interfaces/answer.interface";
 
 
-export default () => {
-
+export default function QNA () {
     const [paragraph, setParagraph] = useState('Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet\'s leading subsidiary and will continue to be the umbrella company for Alphabets Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.');
     const [question, setQuestion] = useState('Who is the CEO of Google?');
     const [predictedAnswer, setPredictedAnswer] = useState<Array<IAnswer>>([]);
